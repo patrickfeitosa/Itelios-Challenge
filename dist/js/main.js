6eb19e31dda7f7fe29773c6b72b1c453a360118d
Patrick Vieira Feitosa <31283687+patrickfeitosa@users.noreply.github.com>
2018-05-27 22:16:51 -0300

    Alimentando o produto visto a partir do JSON
    Alimentando o produto visto a partir do JSON

--- dist/js/main.js
+++ dist/js/main.js
@@ -225,9 +225,31 @@ window.onload = function () {
 	fetch(url)
 		.then(response => response.json()) // retorna uma promise
 		.then(result => {
+
+			//Declaração das variáveis
 			let JSONImage = '//www.itelios.com.br/arquivos/imagens'
-			let nemUrlImg = './img'
-			console.log(result[0].data);
+			let newUrlImg = './img'
+			let htmlProduct = ''
+			let htmlRecommendation = ''
+			let product = result[0].data.item
+			let indexStartPrice = product.productInfo.paymentConditions.indexOf('é') + 1
+			let indexFinishPrice = product.productInfo.paymentConditions.indexOf(',') + 2
+			
+			console.log(indexStartPrice)
+
+			//Percorrendo os Arrays retornados do JSON
+			htmlProduct = 
+			'<img src="' + product.imageName.replace(JSONImage, newUrlImg) + '" alt="' + product.name + '" class="product-img">'+
+			'<p class="info-product">' + product.name + '</p>' +
+			'<p class="product-install">'+				
+				'Por: <span class="product-price">'+ product.price +'</span>' +
+				'<br>'+
+				'ou <span class="product-price-install">'+ product.productInfo.paymentConditions.substring(indexStartPrice, indexFinishPrice) +'</span> sem juros' +
+			'</p>'
+
+			let productContainer = document.querySelector('#currentProduct')
+			productContainer.innerHTML = htmlProduct
+
 			let recommendation = result[0].data.recommendation
 			recommendation.forEach(item => {
 				console.log(item)
